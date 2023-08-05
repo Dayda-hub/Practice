716830681abc179ae1d845be7b25af82fb99b36f
Alexandr Shadrin <shura.shadrin01@mail.ru>
2023-08-05 11:08:25 +0400

    init commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    internal class Program
+    {
+        private static  void MyPracticeMethod()
+        {
+            
+        }
+        static void SayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+        static void Main(string[] args)
+        {
+            
+           SayHello();
+           string someMessage =  Console.ReadLine();
+           Console.WriteLine(someMessage); 
+           Console.ReadLine();  
+
+        }
+    }
+}
